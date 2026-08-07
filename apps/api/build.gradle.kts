7a3469988191a462cde97a75e4460457b49bf637
bynanci <by.nanci.tw@gmail.com>
2026-08-07 09:43:31 +0800

    fix(api): pin the scaffold web dependency explicitly

--- apps/api/build.gradle.kts
+++ apps/api/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     java
     id("org.springframework.boot") version "4.1.0"
-    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "tw.basketball"
@@ -14,16 +13,7 @@ java {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.boot:spring-boot-dependencies:4.1.0")
-    }
+    implementation("org.springframework.boot:spring-boot-starter-web:4.1.0")
 }
 
 tasks.withType<Test> {
