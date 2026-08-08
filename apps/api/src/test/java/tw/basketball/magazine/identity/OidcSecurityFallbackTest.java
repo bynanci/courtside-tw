f62f208747e9ce7aad80afacffd447adfba0c217
bynanci <by.nanci.tw@gmail.com>
2026-08-08 17:08:52 +0800

    test(api): attach Spring Security filters to MockMvc

--- apps/api/src/test/java/tw/basketball/magazine/identity/OidcSecurityFallbackTest.java
+++ apps/api/src/test/java/tw/basketball/magazine/identity/OidcSecurityFallbackTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
@@ -27,7 +28,9 @@ final class OidcSecurityFallbackTest {
 
     @BeforeEach
     void setUp() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilters(webApplicationContext.getBean(FilterChainProxy.class))
+                .build();
     }
 
     @Test
