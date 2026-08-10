df1e4602ab5fb56913e8811f136e0ea73d5ff84f
bynanci <by.nanci.tw@gmail.com>
2026-08-10 09:58:40 +0800

    fix(us3): remove stale JDBC template import

--- apps/api/src/main/java/tw/basketball/magazine/publication/application/EditorialWorkflowConfiguration.java
+++ apps/api/src/main/java/tw/basketball/magazine/publication/application/EditorialWorkflowConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import tools.jackson.databind.ObjectMapper;
 
