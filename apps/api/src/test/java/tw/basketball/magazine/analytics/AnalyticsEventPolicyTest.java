9103ace060a2040a47d378b9fc1d229b47db9304
bynanci <by.nanci.tw@gmail.com>
2026-08-25 11:48:14 +0800

    test(T084): verify all bounded API event shapes

--- apps/api/src/test/java/tw/basketball/magazine/analytics/AnalyticsEventPolicyTest.java
+++ apps/api/src/test/java/tw/basketball/magazine/analytics/AnalyticsEventPolicyTest.java
@@ -20,6 +20,25 @@ class AnalyticsEventPolicyTest {
     }
 
     @Test
+    void acceptsAllFourBoundedEventShapes() {
+        assertTrue(AnalyticsEventPolicy.accepts(
+                "public_issue_view",
+                Map.of("surface", "issue", "content_kind", "issue")));
+        assertTrue(AnalyticsEventPolicy.accepts(
+                "public_search_submitted",
+                Map.of(
+                        "surface", "search",
+                        "query_length_bucket", "3_5",
+                        "result_count_bucket", "1_5")));
+        assertTrue(AnalyticsEventPolicy.accepts(
+                "public_share_started",
+                Map.of(
+                        "surface", "share",
+                        "content_kind", "article",
+                        "share_target", "copy_link")));
+    }
+
+    @Test
     void rejectsUnknownTypesAndRawQueries() {
         assertFalse(AnalyticsEventPolicy.accepts(
                 "unknown_event",
